93f80da39d3d22b6474455b4b15dfa25ed294df2
David Atanda <68742882+Saytzeff-D@users.noreply.github.com>
2022-07-02 18:01:35 +0100

    Update staff.route.js

--- routes/staff.route.js
+++ routes/staff.route.js
@@ -2,17 +2,7 @@ const staffRouter = require("express").Router();
 const staffController = require("../controllers/staff.controller");
 
 staffRouter.post('/register',staffController.registerStaff);
-
-// staffRouter.post('/register',(req,res)=>{
-//     req.body.class=90
-//     res.send(req.body)
-// });
-
-
 staffRouter.post("/login", staffController.login);
-
-
 staffRouter.get("/allstaffs", staffController.allstaffs);
-
 
 module.exports = staffRouter
