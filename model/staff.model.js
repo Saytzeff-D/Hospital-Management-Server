c2b4e5c21e86a066f08e64be9ffe773a0adccff6
David Atanda <68742882+Saytzeff-D@users.noreply.github.com>
2022-07-03 17:10:02 +0100

    quit debug zone, all codes back to normalcy

--- model/staff.model.js
+++ model/staff.model.js
@@ -18,33 +18,33 @@ const staffSchema = mongoose.Schema({
 
 })
 
-// staffSchema.pre('save', (next)=>{
-//   bcrypt.hash(this.password,saltround, (err,hashedPassword)=>{
-//     if(err){
-//         console.log(err)
-//         console.log('cant hash password')
-//     }else{
-//         this.password=hashedPassword
-//     }    
-//     })
-// let file=this.photo
-// cloudinary.v2.upload(file, {public_id:this.fullName}, (err,result)=>{
-//   if(err){
-// console.log('failed to upload')
-// console.log(err)
-//   }
-//   else{
-//     let publicName=this.fullName
-//     let imageUrl=result.secure_url
-//     let splitting=imageUrl.split('upload')
-//     let path=splitting[0]+'upload'
-//     let newImagepath=`${path}/${'w_250,c_scale'}/${publicName}`
-//     console.log(newImagepath)
-//     this.photo=newImagepath
-//     next()
-//   }
-// })
-// })
+staffSchema.pre('save', (next)=>{
+  bcrypt.hash(this.password,saltround, (err,hashedPassword)=>{
+    if(err){
+        console.log(err)
+        console.log('cant hash password')
+    }else{
+        this.password=hashedPassword
+    }    
+    })
+let file=this.photo
+cloudinary.v2.upload(file, {public_id:this.fullName}, (err,result)=>{
+  if(err){
+console.log('failed to upload')
+console.log(err)
+  }
+  else{
+    let publicName=this.fullName
+    let imageUrl=result.secure_url
+    let splitting=imageUrl.split('upload')
+    let path=splitting[0]+'upload'
+    let newImagepath=`${path}/${'w_250,c_scale'}/${publicName}`
+    console.log(newImagepath)
+    this.photo=newImagepath
+    next()
+  }
+})
+})
 
 let StaffModel = mongoose.model("staff", staffSchema);
 module.exports = StaffModel;
