20c3f96f47e7d87a86837bb07fb598a46d9b7078
David Atanda <68742882+Saytzeff-D@users.noreply.github.com>
2022-07-03 17:01:10 +0100

    comment bodyparser.json

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ const cors = require('cors')
 const mongoose = require('mongoose')
 const bodyParser = require('body-parser')
 app.use(bodyParser.urlencoded({extended:true,limit:'50mb'}))
-app.use(bodyParser.json({limit:'50mb'}));
+// app.use(bodyParser.json({limit:'50mb'}));
 app.use(express.json({limit:'50mb'}))
 app.use(cors({origin:'*'}))
 require('dotenv').config()
