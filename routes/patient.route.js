3f9cc0e863588b541cfd6a278c618c9b020c9cd2
Saytzeff-D <davidatanda2020@gmail.com>
2022-07-25 17:26:07 +0100

    I created get and post route to connect frontend

--- routes/patient.route.js
+++ routes/patient.route.js
@@ -1,21 +1,22 @@
 const express = require('express');
 const { addAppointment, fetchAppointments, payAppointmentBill, getPatInfo } = require('../controllers/appointment.controller');
+const { patientPharmBillRecord, payPharmacyBill } = require('../controllers/bills.controller');
 const { authenticatePatient, allPatient, retrievePatientId, updatePat, deletePat, login, registerPatient } = require('../controllers/patient.controller');
-const patientRouter=express.Router()
+const patientRouter = express.Router()
 
 
 patientRouter.post('/register', registerPatient);
-patientRouter.post("/login",  login);
-patientRouter.post("/deletePat",  deletePat);
-patientRouter.post("/updatePat",  updatePat);
-patientRouter.post('/retrievePatientId',  retrievePatientId)
-patientRouter.get("/allPatient",  allPatient);
-patientRouter.get('/authPatient',  authenticatePatient)
+patientRouter.post("/login", login);
+patientRouter.post("/deletePat", deletePat);
+patientRouter.post("/updatePat", updatePat);
+patientRouter.post('/retrievePatientId', retrievePatientId)
+patientRouter.get("/allPatient", allPatient);
+patientRouter.get('/authPatient', authenticatePatient)
 patientRouter.post('/addAppointment', addAppointment)
 patientRouter.post('/fetchAppointments', fetchAppointments)
 patientRouter.post('/payAppointment', payAppointmentBill)
 patientRouter.post('/getInfo', getPatInfo)
-
-
+patientRouter.post('pharmacyBills', patientPharmBillRecord)
+patientRouter.post('payPharmBill', payPharmacyBill)
 
 module.exports = patientRouter
--- routes/staff.route.js
+++ routes/staff.route.js
@@ -4,25 +4,28 @@ const { registerStaff, login, allstaffs, authenticateStaff, getDashboardInfo, ge
 const { addMedicine,allMedicine,updateDrug,delMedicine } = require("../controllers/medicine.controller");
 const { addBirth, fetchAllBirth, addToMortality, fetchAllMortality } = require("../controllers/records.controller");
 const { addPresc, fetchAllPresc } = require("../controllers/prescription.controller");
+const { generatePharmBill, allPharmacyBill } = require("../controllers/bills.controller");
 
 staffRouter.post('/register', registerStaff);
 staffRouter.post("/login", login);
 staffRouter.get("/allStaffs", allstaffs);
 staffRouter.get("/dashboard", authenticateStaff);
 staffRouter.get('/getDashboardInfo',getDashboardInfo)
-staffRouter.get("/allAppointments", allAppointments);
+staffRouter.get("/allAppointments", allAppointments)
 staffRouter.post('/checkAppointment',checkAppointment)
 staffRouter.post('/getPat', getPatDetails)
 staffRouter.post('/updateApp', updateAppointment)
 staffRouter.post('/addMedicine', addMedicine)
-staffRouter.post('/updateMed',updateDrug)
-staffRouter.post('/delMedicine',delMedicine)
+staffRouter.post('/updateMed', updateDrug)
+staffRouter.post('/delMedicine', delMedicine)
 staffRouter.post('/addBirth', addBirth)
 staffRouter.get('/getBirth', fetchAllBirth)
 staffRouter.post('/addDeath', addToMortality)
 staffRouter.get('/getDeath', fetchAllMortality)
-staffRouter.get('/allMedicines',allMedicine)
+staffRouter.get('/allMedicines', allMedicine)
 staffRouter.post('/addPrescription', addPresc)
 staffRouter.get('/allPrescription', fetchAllPresc)
+staffRouter.post('/createPharmBill', generatePharmBill)
+staffRouter.get('/pharmacyBills', allPharmacyBill)
 
 module.exports = staffRouter
